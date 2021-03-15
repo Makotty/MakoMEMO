7809e08412963bba690fb519a1b2c7a11ff577d6
makotty <hal.m.90215@gmail.com>
2021-03-15 23:06:13 +0900

    yarn install

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-// eslint-disable-next-line import/no-unresolved
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {{/* eslint-disable-next-line react/style-prop-object */}}
+      <Text>Welcome! MakoApp!</Text>
       <StatusBar style="auto" />
     </View>
   );
