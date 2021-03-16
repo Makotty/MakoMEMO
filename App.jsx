a51143d49c2305c52dc1a6d5a8110a6d7db35343
makotty <hal.m.90215@gmail.com>
2021-03-16 21:05:07 +0900

    コンテンツStyleSheet更新(ヘッダー、コンテンツ)

--- App.jsx
+++ App.jsx
@@ -4,44 +4,62 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Mako MEMO APP</Text>
-          <Text>LogOut</Text>
+
+      <View style={styles.appbar}>
+        <View style={styles.appbarInnner}>
+          <Text style={styles.appbarTitle}>Mako MEMO APP</Text>
+          <Text style={styles.appbarLogout}>LogOut</Text>
         </View>
       </View>
+
       <View>
-        <View>
+
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020/03/05 05:25</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020/03/05 05:25</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020/03/05 05:25</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
       </View>
+
       <View>
         <Text>+</Text>
       </View>
+
     </View>
   );
 }
@@ -49,8 +67,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dcdcdc',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: 'white',
+    borderBottomWidth: 2,
+    borderColor: 'lime',
+    justifyContent: 'flex-end',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: 'lime',
+    fontWeight: 'bold',
+  },
+  appbarInnner: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appbarLogout: {
+    position: 'absolute',
+    right: 19,
+    bottom: 14,
+    color: 'rgba(0,255,0,0.7)',
+  },
+  memoListItem: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 0.4,
+    borderColor: 'rgba(0,255,0,0.7)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: 'gray',
   },
 });
