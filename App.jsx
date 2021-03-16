e9005e0a99c2919d81a019f9eb8e51375b90deed
makotty <hal.m.90215@gmail.com>
2021-03-16 20:13:37 +0900

    Hallo Componentの作成

--- App.jsx
+++ App.jsx
@@ -2,10 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/componets/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>Small World</Hello>
       <Text>Welcome! MakoApp!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
