8225374d8f9e1641534879971449d81effb50078
makotty <hal.m.90215@gmail.com>
2021-03-16 20:28:02 +0900

    JSX更新要素配置

--- App.jsx
+++ App.jsx
@@ -1,17 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from './src/componets/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello style={{ fontSize: 16 }}>Small World</Hello>
-      <Text>Welcome! MakoApp!</Text>
-      {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto" />
+      <View>
+        <View>
+          <Text>Mako MEMO APP</Text>
+          <Text>LogOut</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020/03/05 05:25</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020/03/05 05:25</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020/03/05 05:25</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
