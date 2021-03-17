4b5fc31a3702ae352111168600d82d60564ea2f4
makotty <hal.m.90215@gmail.com>
2021-03-17 15:35:48 +0900

    MemoListItem.jsx追加(アプリメモ一覧部分)

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import AppBar from './src/componets/AppBar';
+import MemoListItem from './src/componets/MemoListItem';
 
 export default function App() {
   return (
@@ -9,49 +10,7 @@ export default function App() {
 
       <AppBar />
 
-      <View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-      </View>
+      <MemoListItem />
 
       <View style={styles.newMemoButton}>
         <Text style={styles.newMemoPlus}>+</Text>
@@ -65,25 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#dcdcdc',
-  },
-  memoListItem: {
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 0.4,
-    borderColor: 'rgba(0,255,0,0.7)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: 'gray',
   },
   newMemoButton: {
     backgroundColor: 'white',
---
+++ src/componets/MemoListItem.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable linebreak-style */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function MemoListItem() {
+  return (
+    <View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020/03/05 05:25</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  memoListItem: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 0.4,
+    borderColor: 'rgba(0,255,0,0.7)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: 'gray',
+  },
+});
