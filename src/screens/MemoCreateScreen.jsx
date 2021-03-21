108400689075d850db62000d6999d613f3de0ecf
makotty <hal.m.90215@gmail.com>
2021-03-21 21:43:03 +0900

    FireStoreのデータベースにメモの情報を保管

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -5,11 +5,29 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import firebase from 'firebase';
+
 import CircleButton from '../componets/CircleButton';
 import KeyboadSafeView from '../componets/KeyboadSafeView';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+
+  function handlePress() {
+    const db = firebase.firestore();
+    const ref = db.collection('memos');
+    ref.add({
+      bodyText:'Hello',
+    })
+      .then((docRef) => {
+        console.log('Created!', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log('Error!', error);
+      });
+  }
+
   return (
     <KeyboadSafeView style={styles.container}>
 
@@ -18,7 +36,7 @@ export default function MemoCreateScreen(props) {
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </KeyboadSafeView>
   );
