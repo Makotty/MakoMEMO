1b43e139be4af933c77cbad3e3ed9df1db062c61
makotty <hal.m.90215@gmail.com>
2021-03-21 21:56:27 +0900

    入力した文字を保存するように変更

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   TextInput,
@@ -12,13 +12,14 @@ import KeyboadSafeView from '../componets/KeyboadSafeView';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+  const [bodyText, setBodyText] = useState('');
 
   function handlePress() {
     const { currentUser } = firebase.auth();
     const db = firebase.firestore();
     const ref = db.collection(`users/${currentUser.uid}/memos`);
     ref.add({
-      bodyText:'Hello',
+      bodyText,
     })
       .then((docRef) => {
         console.log('Created!', docRef.id);
@@ -33,7 +34,12 @@ export default function MemoCreateScreen(props) {
     <KeyboadSafeView style={styles.container}>
 
       <View style={styles.inputContainer}>
-        <TextInput value="" multiline style={styles.input} />
+        <TextInput
+          value={bodyText}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => { setBodyText(text); }}
+        />
       </View>
       <CircleButton
         name="check"
