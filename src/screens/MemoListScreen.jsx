cac3a1b2bc3f64d81fa85788257d75e2b0aef0ac
makotty <hal.m.90215@gmail.com>
2021-03-21 21:03:33 +0900

    ログアウト機能の実装

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import MemoListItem from '../componets/MemoListItem';
 import CircleButton from '../componets/CircleButton';
+import LogOutButton from '../componets/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+    useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
   return (
     <View style={styles.container}>
 
