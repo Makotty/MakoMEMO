c6836288f596221215f5a4b62a9610938590ba25
makotty <hal.m.90215@gmail.com>
2021-03-23 15:11:01 +0900

    メモの削除機能を追加

--- src/componets/MemoListItem.jsx
+++ src/componets/MemoListItem.jsx
@@ -17,11 +17,36 @@ import {
   instanceOf,
   arrayOf,
 } from 'prop-types';
+import firebase from 'firebase';
+
 import { dateToString } from '../utils';
 
 export default function MemoListItem(props) {
   const { memos } = props;
   const navigation = useNavigation();
+
+  function deleteMemo(id) {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      Alert.alert('メモを削除します。', 'よろしいですか？', [
+        {
+          text: 'キャンセル',
+          onPress: () => {},
+        },
+        {
+          text: '削除する',
+          style: 'destructive',
+          onPress: () => {
+            ref.delete().catch(() => {
+              Alert.alert('削除に失敗しました。');
+            });
+          },
+        },
+      ]);
+    }
+  }
 
   function renderItem({ item }) {
     return (
@@ -35,7 +60,7 @@ export default function MemoListItem(props) {
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
-          onPress={() => { Alert.alert('Are you Sure?'); }}
+          onPress={() => { deleteMemo(item.id); }}
         >
           <Feather name="x" size={16} color="#B0B0B0" />
         </TouchableOpacity>
