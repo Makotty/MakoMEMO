977eb173c3a83d8fea0726c34223b05234145d6b
makotty <hal.m.90215@gmail.com>
2021-03-23 14:31:54 +0900

    メモが0件の場合のUXを追加→新規作成画面へ

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Alert,
+} from 'react-native';
 import firebase from 'firebase';
 
 import MemoListItem from '../componets/MemoListItem';
 import CircleButton from '../componets/CircleButton';
 import LogOutButton from '../componets/LogOutButton';
+import Button from '../componets/Button';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
@@ -41,6 +47,21 @@ export default function MemoListScreen(props) {
     return unsubscribe;
   }, []);
 
+  if (memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <View style={emptyStyles.inner}>
+          <Text style={emptyStyles.title}>メモがありません。作成してください(⌒∇⌒)</Text>
+          <Button
+            style={emptyStyles.button}
+            label="作成する"
+            onPress={() => { navigation.navigate('MemoCreate'); }}
+          />
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
 
@@ -59,5 +80,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e6ffe6',
+  },
+});
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
   },
 });
